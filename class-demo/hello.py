208a5553181254efadd2d6ba038d8bbbdb5a215a
Ephraim67 <jonathaneuphrem@gmail.com>
2025-04-03 12:17:13 +0100

    THis webdeves class code

---
+++ class-demo/hello.py
@@ -0,0 +1,27 @@
+# This line is running python code without a variable
+print("Hello world!")
+print('I am learning python')
+
+
+# Declaring a variable
+f_name = "Ephraim"
+l_name = "Norbert"
+
+# Concatenation (concatenating a variable)
+print(f_name + " " + l_name)
+
+# Types of variables
+# Integer whole number (eg: 0-9)
+# floating-point (float) - Decimam numbers (e.g: 0.1)
+# String (str) - Text Data ("hello", "Python", 'Text')
+# Boolean (bool) - True/false values
+
+age = 18
+sentence = "The legal age to vote in"
+country = "Tanzania"
+
+full_sentence = sentence + " " + country
+
+print(full_sentence + " is " + str(age))
+
+
