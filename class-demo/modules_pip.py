9a67471d4bae7fb86a09e92e964bee3f3713e731
Ephraim67 <jonathaneuphrem@gmail.com>
2025-04-04 12:21:52 +0100

    New phase

---
+++ class-demo/modules_pip.py
@@ -0,0 +1,42 @@
+# Python modules
+"""
+When you install Python, you also install its set of standard libraries and modules that provide you with 
+an extensive range of capabilities. These include built-in data types, exception handling, numeric and 
+math modules, file handling, cryptographic services (critical to hacker and information security pros), 
+internet data handling, and interaction with internet protocols.
+
+Despite all the power offered by these standard libraries and modules, you may need additional third
+party modules. In Python, third-party modules are extensive and one of the primary reasons hackers 
+prefer Python to other programming languages. You can find a comprehensive list of third party-modules 
+at PyPI (the Python Package Index, shown below)
+"""
+
+# Pip
+"""Python has its own package manager (like rpm or apt) specifically for installing and managing Python 
+packages known as pip (Python Installs Packages)."""
+
+
+#  Object-Oriented Programming
+"""OOP revolves 
+around the concept of an object. The object has properties in the form of attributes and states, as well as 
+methods that are actions performed by or on the object."""
+
+"""
+           |---Property (Attribute or state)
+Object ==> |
+           |---Method (Do something function & Procedure)
+
+Objects then are members of a class, which is like a template for creating objects with shared initial 
+variables, properties, and methods. For example, if we have a class called cars, our car (Audi) would be a 
+member of the class of cars. This class would also include other objects/cars, such as Toyota and BMW.
+
+Classes can also have subclasses. Our car class has an Audi subclass, and an object of that subclass might 
+be an Audi A8.
+Each object would have properties (make, model, year, and color) and methods (start, lock, drive, and 
+park).
+In OOP languages such as Python, objects inherit the characteristics of their class; the AudiA8 would 
+inherit the methods (start, lock, drive, and park) from its class “car.”
+ These OOP concepts are critical to understanding how Python works, as you will see as you progress 
+through this chapter, and your Python skills develop.
+"""
+
